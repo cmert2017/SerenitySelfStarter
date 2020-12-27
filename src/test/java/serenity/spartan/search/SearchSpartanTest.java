b43fc2b5523cce1d66cd3a4974a14512ce849554
cmert2017 <cihanmert2075@gmail.com>
2020-12-27 10:50:20 -0500

    Adding Serenity get /spartans/search End point test

--- src/test/java/serenity/spartan/search/SearchSpartanTest.java
+++ src/test/java/serenity/spartan/search/SearchSpartanTest.java
@@ -1,4 +1,31 @@
 package serenity.spartan.search;
 
-public class SearchSpartanTest {
+import net.serenitybdd.junit5.SerenityTest;
+import net.serenitybdd.rest.Ensure;
+import net.serenitybdd.rest.SerenityRest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import serenity.utility.SpartanTestBase;
+
+import static net.serenitybdd.rest.SerenityRest.given;
+
+@SerenityTest
+public class SearchSpartanTest extends SpartanTestBase {
+
+    @DisplayName("Authenticated user should be able to search")
+    @Test
+    public void testSearch(){
+        given()
+                .auth().basic("admin","admin")
+                .queryParam("gender","Female")
+                .queryParam("nameContains","e").
+        when()
+                .get("/spartans/search");
+
+
+        Ensure.that("Request was successful",
+                vResponse -> vResponse.statusCode(200));
+
+    }
 }
